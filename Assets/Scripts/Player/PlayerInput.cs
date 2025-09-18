a58b55fa6d64878800fca7dd82bfac38f647a933
postall_mcs <postall_mcs@mail.ru>
2025-09-18 17:59:31 +0500

    Added rotaion camera
    Добавлены методы для вращения камеры.
    1. В отдельный метод выделено управление поворотом камеры а именно HandleRotation(), проверка при нажатии клавиш PageUp и PageDown. Это вынесено в метод IsRotationStartTime().
    Логика такая же как и при реализации управления зумом камеры.

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -11,33 +11,86 @@ namespace GameDevTV.RTS
         [SerializeField] private CinemachineCamera _cinemachineCamera;
         [SerializeField] private float _keyboardPanSpeed = 15.0f;
         [SerializeField] private float _zoomSpeed = 1.0f;
+        [SerializeField] private float _rotationSpeed = 1.0f;
         [SerializeField] private float _minZoomDistance = 7.5f;
 
         private CinemachineFollow _cinemachineFollow;
         private Vector2 _moveAmount;
         private float _zoomStartTime;
+        private float _rotationStartTime;
         private Vector3 _startingFollowOffset;
+        private float _maxRotationAmount;
 
         private void Awake()
         {
             _moveAmount = Vector2.zero;
             _cinemachineFollow = _cinemachineCamera.GetComponent<CinemachineFollow>();
             _startingFollowOffset = _cinemachineFollow.FollowOffset;
+            _maxRotationAmount = Mathf.Abs(_cinemachineFollow.FollowOffset.z);
         }
 
         private void Update()
         {
             HandlePanning();
             HandleZooming();
+            HandleRotation();
+        }
+
+        private void HandleRotation()
+        {
+            if(IsRotationStartTime())
+                _rotationStartTime = Time.time;
+
+            float ratationTime = Mathf.Clamp01((Time.time - _rotationStartTime) * _rotationSpeed);
+            Vector3 targetFollowOffset = CalculateDesiredOffsetRotation();
+            _cinemachineFollow.FollowOffset = Vector3.Slerp
+                (
+                    _cinemachineFollow.FollowOffset,
+                    targetFollowOffset,
+                    ratationTime
+                );
+        }
+
+        private Vector3 CalculateDesiredOffsetRotation()
+        {
+            if (Keyboard.current.pageDownKey.isPressed)
+                return new Vector3
+                    (
+                        _maxRotationAmount,
+                        _cinemachineFollow.FollowOffset.y,
+                        0
+                    );
+            else if (Keyboard.current.pageUpKey.isPressed)
+                return new Vector3
+                    (
+                        -_maxRotationAmount,
+                        _cinemachineFollow.FollowOffset.y,
+                        0
+                    );
+            else
+                return  new Vector3
+                    (
+                        _startingFollowOffset.x,
+                        _cinemachineFollow.FollowOffset.y,
+                        _startingFollowOffset.z
+                    );
+        }
+
+        private bool IsRotationStartTime()
+        {
+            return Keyboard.current.pageUpKey.wasPressedThisFrame
+                || Keyboard.current.pageDownKey.wasPressedThisFrame
+                || Keyboard.current.pageUpKey.wasReleasedThisFrame
+                || Keyboard.current.pageDownKey.wasReleasedThisFrame;
         }
 
         private void HandleZooming()
         {
-            if (IsZoomInputChanged())
+            if (IsZoomStartTime())
                 _zoomStartTime = Time.time;
 
             float zoomTime = Mathf.Clamp01((Time.time - _zoomStartTime) * _zoomSpeed);
-            Vector3 targetFollowOffset = CalculateDesiredOffset();
+            Vector3 targetFollowOffset = CalculateDesiredOffsetZoom();
             _cinemachineFollow.FollowOffset = Vector3.Slerp
                 (
                     _cinemachineFollow.FollowOffset,
@@ -46,7 +99,7 @@ namespace GameDevTV.RTS
                 );
         }
 
-        private Vector3 CalculateDesiredOffset()
+        private Vector3 CalculateDesiredOffsetZoom()
         {
             return Keyboard.current.endKey.isPressed ?
                 new Vector3
@@ -64,7 +117,7 @@ namespace GameDevTV.RTS
                      );
         }
 
-        private bool IsZoomInputChanged()
+        private bool IsZoomStartTime()
         {
             return Keyboard.current.endKey.wasPressedThisFrame
                 || Keyboard.current.endKey.wasReleasedThisFrame;
